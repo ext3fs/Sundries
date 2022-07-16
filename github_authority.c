3d83acac33070c6df8ec064fba9469a0ef10eccd
Ilyoung Jung <ext3fs@hanmail.net>
2022-07-17 02:29:54 +0900

    6

--- github_authority.c
+++
@@ -1,5 +0,0 @@
-sudo git config --global --unset credential.helper
-git config --global push.default simple
-git config --global credential.helper store -> 영구저장
-vi C:/Users/ext3fs/.git-credentials
-	> http:<user_id>:<user_access_token>@github.com -> 토큰 저장
